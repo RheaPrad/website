a08ffc678b871a00a6b1e091c85032ec6e71df29
Aman Bhargava <amanbhargava2001@gmail.com>
2026-02-21 18:03:58 +0530

    chore: WIP sync

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -62,37 +62,35 @@ export const GET: RequestHandler = async () => {
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
   const xml = `
-		<?xml version="1.0" encoding="UTF-8" ?>
-		<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-			<channel>
-				<title>${title}</title>
-				<description>${siteDescription}</description>
-				<link>${siteURL}</link>
-				<atom:link href="${siteURL}/rss.xml" rel="self" type="application/rss+xml"/>
-				${posts
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+            <channel>
+                <title>${title}</title>
+                <description>${siteDescription}</description>
+                <link>${siteURL}</link>
+                <atom:link href="${siteURL}/rss.xml" rel="self" type="application/rss+xml"/>
+                ${posts
       .map((post) => {
         const imageHtml = post.imageSrc
-          ?\`<img src="\${post.imageSrc}" alt="\${post.title}" /><br/>\`
-							: '';
-						return \`
-						<item>
-							<title><![CDATA[\${post.title}]]></title>
-							<description><![CDATA[\${imageHtml}\${post.description}]]></description>
-							<link>\${post.url}</link>
-							<guid isPermaLink="true">\${post.url}</guid>
-							<pubDate>\${new Date(post.date).toUTCString()}</pubDate>
-						</item>
-					\`;
-					})
-					.join('')}
-			</channel>
-		</rss>
-	`.trim();
+          ? '<img src="' + post.imageSrc + '" alt="' + post.title + '" /><br/>'
+          : '';
+        return '<item>' +
+          '<title><![CDATA[' + post.title + ']]></title>' +
+          '<description><![CDATA[' + imageHtml + post.description + ']]></description>' +
+          '<link>' + post.url + '</link>' +
+          '<guid isPermaLink="true">' + post.url + '</guid>' +
+          '<pubDate>' + new Date(post.date).toUTCString() + '</pubDate>' +
+          '</item>';
+      })
+      .join('')}
+            </channel>
+        </rss>
+    `.trim();
 
-        return new Response(xml, {
-          headers: {
-            'Cache-Control': 'max-age=0, s-maxage=3600',
-            'Content-Type': 'application/xml',
-          }
-        });
-      };
+  return new Response(xml, {
+    headers: {
+      'Cache-Control': 'max-age=0, s-maxage=3600',
+      'Content-Type': 'application/xml',
+    }
+  });
+};
